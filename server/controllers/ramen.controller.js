002eb2d1134a0f6b856a750f9a4ccccfd62693a5
tarmacpro <jimmysyim@gmail.com>
2022-03-24 22:00:11 -0700

    added ramen.controller.js and ramen.routes.js

---
+++ server/controllers/ramen.controller.js
@@ -0,0 +1,26 @@
+const { update } = require('../models/ramen.model');
+const Ramen = require('../models/ramen.model');
+
+module.exports.createRamen = (req, res) => {
+    Ramen.create(req.body)
+        .then(ramen => res.json(ramen))
+        .catch(err => res.json(err))
+}
+
+module.exports.getOneRamen = (req, res) => {
+    Ramen.findOne({_id:req.params.id})
+        .then(ramen => res.json(ramen))
+        .catch(err => res.json(err))
+}
+
+module.exports.deleteRamen = (req, res) => {
+    Ramen.deleteOne({_id: req.params.id})
+        .then(deleteRamen => res.json(deleteRamen))
+        .catch(err => res.json(err))
+}
+
+module.exports.updateRamen = (req, res) => {
+    Ramen.findOneAndUpdate({_id: req.params.id})
+        .then(updateRamen => res.json(updateRamen))
+        .catch(err => res.json(err))
+}
---
+++ server/routes/ramen.routes.js
@@ -0,0 +1,8 @@
+const RamenController = require('../controllers/ramen.controller');
+
+module.exports = (app) => {
+    app.get("/ramen/:id", RamenController.getOneRamen);
+    app.delete("/ramen/:id", RamenController.deleteRamen);
+    app.put("/ramen/:id/edit", RamenController.updateRamen);
+    app.post("/ramen/new", RamenController.createRamen);
+}
